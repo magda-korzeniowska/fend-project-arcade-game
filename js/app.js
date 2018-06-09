1d767bbace6c4403270dfce06fffa99b76c1f933
magda-korzeniowska <m.korzeniowska@tlen.pl>
2018-06-09 11:13:34 +0200

    Add method updating enemy's position

--- js/app.js
+++ js/app.js
@@ -14,16 +14,24 @@ class Enemy {
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
+  update(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-};
+    this.x += this.speed * dt;
+
+    // Makes enemies go to the left side of canvas after reaching
+    // the right end of canvas
+    if (this.x >= 500) {
+      this.x = -100;
+    }
+  }
 
 // Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
+  render() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+  }
+}
 
 // Now write your own player class
 // This class requires an update(), render() and
