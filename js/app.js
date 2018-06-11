2de1982c0ec4b61249ee84f5f29be3e5a081e191
magda-korzeniowska <m.korzeniowska@tlen.pl>
2018-06-11 14:54:32 +0200

    Update comments in js file

--- js/app.js
+++ js/app.js
@@ -1,48 +1,50 @@
 let modal = document.querySelector('.modal');
 let close = document.querySelector('.close');
 let again = document.querySelector('.play-again');
+let points = 0;
+let pointsCount = document.querySelector('.points');
+
+/*
+ * Enemy section
+ */
 
 // Enemies our player must avoid
 class Enemy {
   constructor(x,y) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
     this.x = x;
     this.y = y;
     this.speed = Math.floor(Math.random() * (350 - 100) + 100);
     this.sprite = 'images/enemy-bug.png';
   }
 
-// Update the enemy's position, required method for game
+// Update the enemy's position
 // Parameter: dt, a time delta between ticks
   update(dt) {
-    // You should multiply any movement by the dt parameter
+    // Multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
     this.x += this.speed * dt;
 
-    // Makes enemies go to the left side of canvas after reaching
+    // Make enemies go to the left side of canvas after reaching
     // the right end of canvas
     if (this.x >= 500) {
       this.x = -100;
     }
 
-    // Checks for collision with enemies and walls
+    // Check for collision with enemies and walls
     checkCollision(this);
   }
 
-// Draw the enemy on the screen, required method for game
+// Draw the enemy on the screen
   render() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
   }
 }
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+/*
+ * Player section
+ */
+
 class Player {
   constructor(x,y) {
     this.x = x;
@@ -59,6 +61,7 @@ class Player {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
   }
 
+  // enable player to move - method for arrow keys
   handleInput(keyPress) {
     switch(keyPress) {
       case 'left':
@@ -101,17 +104,29 @@ class Player {
   }
 }
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. You don't need to modify this.
+document.addEventListener('keyup', function(e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+
+    player.handleInput(allowedKeys[e.keyCode]);
+});
+
+/*
+ * Objects section
+ */
+
+// Instantiate all objects
 let enemy1 = new Enemy(-50, 60);
 let enemy2 = new Enemy(-100, 145);
 let enemy3 = new Enemy(-150, 230);
 let allEnemies = [enemy1, enemy2, enemy3];
 let player = new Player(200, 400);
-
-let points = 0;
-let pointsCount = document.querySelector('.points');
 
 // reset player
 function resetPlayer() {
@@ -135,21 +150,8 @@ function checkCollision(oneEnemy) {
     }
 }
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
-
-    player.handleInput(allowedKeys[e.keyCode]);
-});
-
 function winGame() {
-  if (points === 1) {
+  if (points === 5) {
     //shows congratulations popup window
     modal.style.display = "block";
     playAgain();
@@ -157,7 +159,7 @@ function winGame() {
   }
 }
 
-//closes popup window, starts new game
+// close popup window, start new game
 function closePopUp() {
   close.addEventListener('click', function(event) {
     modal.style.display = "none";
@@ -167,7 +169,7 @@ function closePopUp() {
   });
 }
 
-//starts new game (play again button)
+// start new game (play again button)
 function playAgain() {
   again.addEventListener('click', function(event) {
      modal.style.display = "none";
